7b2847dd642fd341e822cf8e3165ed50bfb84286
benso@Benadesigns <mbuguabenson2020@gmail.com>
2026-01-11 22:40:27 +0300

    fix: remove unused useDevice import from chart-modal

--- src/pages/chart/chart-modal/chart-modal.tsx
+++ src/pages/chart/chart-modal/chart-modal.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import { observer } from 'mobx-react-lite';
-import { Loader, useDevice } from '@deriv-com/ui';
+import { Loader } from '@deriv-com/ui';
 import ChartModalDesktop from './chart-modal-desktop';
 
 export const ChartModal = observer(() => {
